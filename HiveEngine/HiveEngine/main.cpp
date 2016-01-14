fc8f0627494341401ef6f887577fc6e1925fba32
Jordan Taylor <football80@gmail.com>
2016-01-13 18:33:40 -0700

    Got the project working and drawing a circle!

---
+++ HiveEngine/HiveEngine/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+	cout << "Hello World!" << endl;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
